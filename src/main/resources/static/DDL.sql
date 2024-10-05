370c38f1d256331121e40a40db3fda3e35364b47
Pedro.Arango <Pedro.Arango@nordstrom.com>
2024-10-05 12:33:04 -0500

    Module1: Changed DDL from enum to varchar

--- src/main/resources/static/DDL.sql
+++ src/main/resources/static/DDL.sql
@@ -1,15 +1,16 @@
--- Creation of crew members table
-CREATE TYPE enum_gender AS ENUM ('Male','Female','Other');
-CREATE TYPE enum_civil_status AS ENUM ('Married','Single','Widowed','Divorced','Other');
+--DROP TABLE crew_member;
+--DROP TABLE spaceship;
+--DROP TABLE exploration;
 
+-- Creation of crew members table
 CREATE TABLE crew_member(
 	id varchar(30) PRIMARY KEY,
 	family_id integer NOT NULL,
 	name varchar(60) NOT NULL,
 	surname varchar(60) NOT NULL,
 	age smallint NOT NULL,
-	gender enum_gender,
-	civil_status enum_civil_status,
+	gender varchar(20),
+	civil_status varchar(20),
 	children smallint,
 	salary decimal,
 	trips_achieved smallint
@@ -25,8 +26,8 @@ CREATE TABLE spaceship(
 -- Creation of exploration table
 CREATE TABLE exploration(
 	id integer,
-	spaceship_id integer NOT NULL,
 	cabin_id integer NOT NULL,
+	spaceship_id integer NOT NULL,
 	crew_member_id varchar(30) NOT NULL,
 	PRIMARY KEY(id, cabin_id),
 	FOREIGN KEY (spaceship_id) REFERENCES spaceship(id) ,
